50fe17a4d62f5a7f99f7bb76ad33c15225e23c9b
LeelooE <leeloonita@gmail.com>
2018-07-22 14:31:51 -0600

    cleaning app.js

--- src/app.js
+++ src/app.js
@@ -5,12 +5,7 @@ import AppRouter, { history } from "./routers/AppRouter";
 import configureStore from "./store/configureStore";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
-import LoadingPage from "./components/LoadingPage";
-import { login, logout } from "./actions/auth";
 import "react-dates/lib/css/_datepicker.css";
-import { firebase } from "./firebase/firebase";
-import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
-import Fonts from './fonts';
 
 const store = configureStore();
 const jsx = (
